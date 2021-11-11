4c019a0e3c7ef6792ab9b883e2c054b0b65f9ec9
Devin Winando <devin.winando13@gmail.com>
2021-11-11 20:11:09 +0700

    initial commit

---
+++ src/components/component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Component() {
+  return (
+    <div>
+      <h1 className="text-purple-600 text-5xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default Component;
