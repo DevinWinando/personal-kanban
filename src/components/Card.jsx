1d843c588db07d18c67d1c7556466e010ac45768
Devin Winando <devin.winando13@gmail.com>
2021-11-12 22:00:07 +0700

    add: input to localStorage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Input from "./Input";
 
 function Card(props) {
+  let [todos, setTodos] = useState();
+
   return (
     <div className="card bordered h-4/5 mt-10 ml-10">
       <div className="card-body">
         <h1 className="card-title">{props.title}</h1>
         <hr className="mb-4" />
-        <div class="collapse border rounded-md border-base-300 collapse-arrow">
+        <div className="collapse border rounded-md border-base-300 collapse-arrow">
           <input type="checkbox" />
           <div className="collapse-title text-xl font-medium">First Task</div>
           <div className="collapse-content">
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Input() {
+  let [todos, setTodos] = useState({
+    title: "",
+    description: "",
+  });
+
+  let handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    todos[name] = value;
+    setTodos(todos);
+  };
+
+  let save = (e) => {
+    e.preventDefault();
+    localStorage.setItem("personalKanban", JSON.stringify(todos));
+  };
+
   return (
-    <div className="form-control mt-5">
-      <div className="border-2 border-gray-900 rounded-md">
-        <input type="text" placeholder="Title" className="input rounded-b-none focus:ring-0 w-full" />
-        <textarea className="textarea h-28 max-h-64 focus:ring-0 rounded-t-none w-full" placeholder="Description" name="desc"></textarea>
+    <form action="" onSubmit={save}>
+      <div className="form-control mt-5">
+        <div className="border-2 border-gray-900 rounded-md">
+          <input type="text" placeholder="title" className="input rounded-b-none focus:ring-0 w-full" onChange={handleChange} name="title" />
+          <textarea className="textarea h-28 max-h-64 focus:ring-0 rounded-t-none w-full" placeholder="Description" name="description" onChange={handleChange}></textarea>
+        </div>
+        <div className="d-flex">
+          <button className="btn btn-active w-1/4 mt-3 mr-3" aria-pressed="true" type="submit">
+            Save
+          </button>
+          <button className="btn btn-active btn-secondary w-1/4 mt-3" aria-pressed="true">
+            Cancel
+          </button>
+        </div>
       </div>
-      <div className="d-flex">
-        <button className="btn btn-active w-1/4 mt-3 mr-3" aria-pressed="true">
-          Save
-        </button>
-        <button className="btn btn-active btn-secondary w-1/4 mt-3" aria-pressed="true">
-          Cancel
-        </button>
-      </div>
-    </div>
+    </form>
   );
 }
 
