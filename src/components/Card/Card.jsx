2b8d3148e266f4ff8e1f4aa5fb2913186cb0a97b
Devin Winando <devin.winando13@gmail.com>
2021-11-20 11:26:59 +0700

    fix: dnd error

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import CardHeader from "./_CardHeader";
 import CardBody from "./_CardBody";
 import CardFooter from "./_CardFooter";
+import { Droppable } from "react-beautiful-dnd";
 
 function Card(props) {
-  const { category, title, index } = props;
+  const { category, title } = props;
 
   let [todos, setTodos] = useState([]);
 
@@ -17,17 +18,25 @@ function Card(props) {
   }, []);
 
   return (
-    <div className="card bordered h-4/5 mt-10 w-96 ml-10 static">
-      <CardHeader title={title} />
-      <div className="px-8 h-full card-body overflow-y-overlay">
-        {todos.map((todo) => {
-          const { id, title, desc } = todo;
+    <div className="div">
+      <Droppable droppableId={category}>
+        {(provided) => {
+          return (
+            <div className="card bordered h-4/5 mt-10 w-96 ml-10 static" ref={provided.innerRef} {...provided.droppableProps}>
+              <CardHeader title={title} />
+              <div className="px-8 h-full card-body overflow-y-overlay">
+                {todos.map((todo, index) => {
+                  const { id, title, desc } = todo;
 
-          return <CardBody key={id} id={id} title={title} desc={desc} index={index} category={category} todoCategory={todo.category} />;
-        })}
-      </div>
-      {category === "task" ? <CardFooter /> : ""}
-      {props.children}
+                  return <CardBody key={id} id={id} index={index} title={title} desc={desc} category={category} todoCategory={todo.category} />;
+                })}
+              </div>
+              {category === "task" ? <CardFooter /> : ""}
+              {provided.placeholder}
+            </div>
+          );
+        }}
+      </Droppable>
     </div>
   );
 }
--- src/components/Card/_CardBody.jsx
+++ src/components/Card/_CardBody.jsx
@@ -5,26 +5,28 @@ import FormEdit from "./_FormEdit";
 import { Draggable } from "react-beautiful-dnd";
 
 function _cardBody(props) {
-  const { id, title, desc, category, todoCategory, index } = props;
+  const { id, index, title, desc, category, todoCategory } = props;
   let [showMenu, setShowMenu] = useState(false);
   let [showFormEdit, setShowFormEdit] = useState(false);
 
   return (
-    <Draggable draggableId={id} index={index}>
-      {(provided) => (
-        <div className="wrapper" ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
-          {showMenu === true ? <Menu setShowMenu={setShowMenu} setShowFormEdit={setShowFormEdit} id={id} /> : ""}
+    <Draggable draggableId={id.toString()} index={index}>
+      {(provided) => {
+        return (
+          <div className="wrapper" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            {showMenu === true ? <Menu setShowMenu={setShowMenu} setShowFormEdit={setShowFormEdit} id={id} /> : ""}
 
-          {(() => {
-            if (category === todoCategory && showFormEdit === true) {
-              return <FormEdit id={id} title={title} desc={desc} todoCategory={todoCategory} setShowMenu={setShowMenu} setShowFormEdit={setShowFormEdit} />;
-            } else if (category === todoCategory) {
-              return <MiniCard id={id} title={title} desc={desc} setShowMenu={setShowMenu} />;
-            }
-            return "";
-          })()}
-        </div>
-      )}
+            {(() => {
+              if (category === todoCategory && showFormEdit === true) {
+                return <FormEdit id={id} title={title} desc={desc} todoCategory={todoCategory} setShowMenu={setShowMenu} setShowFormEdit={setShowFormEdit} />;
+              } else if (category === todoCategory) {
+                return <MiniCard id={id} title={title} desc={desc} setShowMenu={setShowMenu} />;
+              }
+              return "";
+            })()}
+          </div>
+        );
+      }}
     </Draggable>
   );
 }
